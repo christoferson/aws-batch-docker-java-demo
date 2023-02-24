94010d8b0b096e8c5d62b839b7311545468846e8
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-02-24 23:56:09 +0900

    refactor

--- src/main/java/demo/BasicProcessor.java
+++ src/main/java/demo/BasicProcessor.java
@@ -6,37 +6,65 @@ import software.amazon.awssdk.regions.Region;
 public class BasicProcessor {
 
 	public static void main(String[] args) {
+
+		demoArguments(args);
+
+		demoEnvironmentVariables();
+
+		demoSimulateWork();
+		
+		demoS3ListBucket();
+
+	}
+	
+	private static void demoArguments(String[] args) {
+		
 		System.out.printf("Args.Length: %s %n", args.length);
 		for (String arg : args) {
 			System.out.printf("Arg[x]: %s %n", arg);
 		}
+
+	}
+	
+	private static void demoEnvironmentVariables() {
+		
 		System.out.printf("Env.APP_ENV: %s %n", System.getenv("APP_ENV"));
+		
+	}
+	
+	private static void demoSimulateWork() {
+		
 		try {
 			for (int i = 0; i < 10; i++) {
 				System.out.println("Foo " + i);
 				Thread.sleep(1000);
 				System.out.println("Bar " + i);
 			}
-			
-			process();
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
-
+		
 	}
 
-	private static void process() {
+	private static void demoS3ListBucket() {
+		
+		try {
 
-		String awsRegion = System.getenv("APP_AWS_REGION");
+			String awsRegion = System.getenv("APP_AWS_REGION");
+			
+			Region region = (awsRegion == null)? Region.US_EAST_1 : Region.of(awsRegion);
+			
+			System.out.printf("Listing Buckets: Region=%s %n", region);
+	
+			AwsS3Client s3 = new AwsS3Client(region);
+			
+			s3.list();
 		
-		Region region = (awsRegion == null)? Region.US_EAST_1 : Region.of(awsRegion);
-
-		AwsS3Client s3 = new AwsS3Client(region);
-		
-		s3.list();
-		
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 }
