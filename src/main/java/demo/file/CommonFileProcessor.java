6ac27c63dad37a7d152cc7793bb7298e26003ecc
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-02-25 14:58:06 +0900

    Add CharSet

--- src/main/java/demo/file/CommonFileProcessor.java
+++ src/main/java/demo/file/CommonFileProcessor.java
@@ -1,14 +1,16 @@
 package demo.file;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class CommonFileProcessor {
 
 	public static void demoReadFileLineByLine(String path) {		
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(new File(path), StandardCharsets.UTF_8))) {
 			String line = reader.readLine();
 
 			while (line != null) {
@@ -21,6 +23,7 @@ public class CommonFileProcessor {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
+
 	}
 	
 }
