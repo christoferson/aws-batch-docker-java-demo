f8250d2a148e60a4c4f954f29d7101ed2a5dbafe
christoferson <sweetsorrow1981@yahoo.co.jp>
2023-02-26 18:10:22 +0900

    Made XML Processor Generic

--- src/main/java/demo/xml/CommonXmlProcessor.java
+++ src/main/java/demo/xml/CommonXmlProcessor.java
@@ -5,26 +5,29 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-import demo.model.Catalog;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Unmarshaller;
 
 public class CommonXmlProcessor {
 	
-	public static void demoParseXmlFile(String path) {
+	public static <T> List<T> demoParseXmlFile(String path, Class<T> clazz) {
+		
+		List<T> elements = new ArrayList<>();
 
 		try (BufferedReader reader = new BufferedReader(new FileReader(new File(path), StandardCharsets.UTF_8))) {
 			
-			JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+			JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
 			
 			String line = null;
 
 			while ((line = reader.readLine()) != null) {
 				System.out.println(line);
-				Catalog catalog = parse(jaxbContext, line);
-				System.out.printf("%s %n", catalog);
-				
+				T element = parse(jaxbContext, line, clazz);
+				System.out.printf("%s %n", element);
+				elements.add(element);
 			}
 
 			reader.close();
@@ -32,24 +35,27 @@ public class CommonXmlProcessor {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
+		
+		return elements; 
+
 	}
 	
-	public static Catalog parse(JAXBContext jaxbContext, String xml) {	
+	public static <T> T parse(JAXBContext jaxbContext, String xml, Class<T> clazz) {	
 		
-		Catalog catalog = null;
+		T element = null;
 
 		try {
 
 			StringReader sr = new StringReader(xml);
 			
 			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-			catalog = (Catalog) unmarshaller.unmarshal(sr);
+			element = clazz.cast(unmarshaller.unmarshal(sr));
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
 	
-		return catalog;
+		return element;
 	}
 
 }
